a78ffa5ed2f2cfa1f1eb5185ad52d01a7445fb38
yanJyli <85889195+yanJyli@users.noreply.github.com>
2022-06-24 21:49:52 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <div className="">
+          <h2>Find Your Book</h2>
+          <input type="text" placeholder="Enter Your Book Name" />
+          <button className="">Search</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
