c555ac7390896210f95b7a40ec688787235a9fc4
yanJyli <85889195+yanJyli@users.noreply.github.com>
2022-06-29 18:30:57 +0300

    next

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+
+import Card from "./Components/Card";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+  const [apiKey, setApiKey] = useState(
+    "AIzaSyAA5TVVlnC9_B9IBbytM9nFpzuBBY8YfiI"
+  );
+
+  const searchBook = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=" +
+            search +
+            "&key=" +
+            apiKey +
+            "&maxResults=40"
+        )
+        .then((res) => setData(res.data.items));
+    }
+  };
+
   return (
     <>
       <div className="header">
         <div className="">
           <h2>Find Your Book</h2>
-          <input type="text" placeholder="Enter Your Book Name" />
+          <input
+            type="text"
+            placeholder="Enter Your Book Name"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={searchBook}
+          />
           <button className="">Search</button>
         </div>
       </div>
+
+      <div>{<Card book={bookData} />}</div>
     </>
   );
 }
